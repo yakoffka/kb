5fedfe8a943bbfc8592a55869d09b78e1f11cab3
yakoffka <yagithub@mail.ru>
2021-01-26 21:05:19 +0300

    Добавление атрибута  target="_blank" к ссылкам на документы

--- resources/views/static_pages/parts/document.blade.php
+++ resources/views/static_pages/parts/document.blade.php
@@ -1,15 +1,17 @@
 <div class="col-lg-4 col-md-4">
     <div class="h_blog_post_item blog_item">
-        <a href="{{ asset('storage/documents/' . $dataDocument['resource']) }}">
+        <a href="{{ asset('storage/documents/' . $dataDocument['resource']) }}" target="_blank">
             <h5 class="text-center">{{ $dataDocument['header'] }}</h5>
         </a>
-        <a href="{{ asset('storage/documents/' . $dataDocument['resource']) }}" class="post_img">
+        <a href="{{ asset('storage/documents/' . $dataDocument['resource']) }}" class="post_img" target="_blank">
             <img src="{{ asset('storage/documents/' . $dataDocument['image']) }}"
                  alt="{{ $dataDocument['header'] }}" {{--width="370"--}} height="218"/>
         </a>
 
         <div class="post_content">
-            <a href="{{ asset('storage/documents/' . $dataDocument['resource']) }}" class="h_post_date">{{ $dataDocument['downloaded_at'] }}</a>
+            <a href="{{ asset('storage/documents/' . $dataDocument['resource']) }}" class="h_post_date" target="_blank">
+                {{ $dataDocument['downloaded_at'] }}
+            </a>
             {{--<div class="h_post_info">
                 <a href="#"><i class="far fa-heart"></i>72</a>
                 <a href="#"><i class="far fa-comment"></i>29</a>
